0eae512dc350c0e890a367e8c07b68419da53986
me_madness <p_rangel@abv.bg>
2024-05-14 19:30:00 +0200

    adding new file 'How To Create A Type'

---
+++ Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/how_to_create_a_type.sql
@@ -0,0 +1,18 @@
+# How To Create aa Type
+
+CREATE TYPE address AS (
+	street VARCHAR(50),
+	city VARCHAR(50),
+	postalCode CHAR(4)
+);
+
+CREATE TABLE customers(
+	id SERIAL PRIMARY KEY,
+	customer_name VARCHAR(150),
+	customer_address address
+);
+
+INSERT INTO
+	customers(customer_name, customer_address)
+VALUES
+	('Rangel', ('8, rue Ermesinde', '6437'));
