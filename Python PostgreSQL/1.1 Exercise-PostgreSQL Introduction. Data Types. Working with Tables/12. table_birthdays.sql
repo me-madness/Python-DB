83e8345039b60077bf15b74fa7c1fea309ab213a
me_madness <p_rangel@abv.bg>
2024-05-14 18:49:01 +0200

    12 table birthdays is ready

--- Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/12. table_birthdays.sql
+++ Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/12. table_birthdays.sql
@@ -1,2 +1,10 @@
 # Table Birthdays
 
+CREATE TABLE minions_birthdays(
+	id SERIAL UNIQUE NOT NULL,
+	name VARCHAR(50),
+	date_of_birth DATE,
+	age INTEGER,
+	present VARCHAR(100),
+	party timestamptz
+);
