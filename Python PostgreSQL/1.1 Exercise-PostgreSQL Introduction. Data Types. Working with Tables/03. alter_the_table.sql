4bcc2636d1346578e57aa9539087f06803b08dcb
me_madness <p_rangel@abv.bg>
2024-05-14 18:25:42 +0200

    08 add constraints is ready

--- Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/03. alter_the_table.sql
+++ Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/03. alter_the_table.sql
@@ -1,6 +1,6 @@
 # Alter the Table
 
-ALTER TABLE minions_nfo
+ALTER TABLE minions_info
 ADD COLUMN code CHAR(4),
 ADD COLUMN task TEXT,
 ADD COLUMN salary NUMERIC(8, 3);
--- Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/08. add_constraints.sql
+++ Python PostgreSQL/1.1 Exercise-PostgreSQL Introduction. Data Types. Working with Tables/08. add_constraints.sql
@@ -1,2 +1,9 @@
 # Add Constraints
 
+ALTER TABLE minions_info
+	
+ADD CONSTRAINT unique_containt
+UNIQUE(id, email),
+
+ADD CONSTRAINT banana_check
+CHECK (banana > 0);
