2ecfc36764b295cc9d3b4f154c963c26de204b49
me_madness <p_rangel@abv.bg>
2024-05-16 20:05:06 +0200

    task 06 is done

--- Python PostgreSQL/2.0 Basic CRUD/06. delete_from_table.sql
+++ Python PostgreSQL/2.0 Basic CRUD/06. delete_from_table.sql
@@ -1,2 +1,9 @@
 -- Delete from Table
 
+DELETE FROM employees
+WHERE department_id = 2 or department_id = 1
+--WHERE department_id BETWEEN 1 and 2
+--WHERE department_id IN (1, 2)
+;
+
+SELECT * FROM employees
