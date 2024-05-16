30c85d287715d703fe0283f14f07e58aa8dd41c0
me_madness <p_rangel@abv.bg>
2024-05-16 19:48:35 +0200

    task 04 is done

--- Python PostgreSQL/2.0 Basic CRUD/04. insert_data_into_employees_table.sql
+++ Python PostgreSQL/2.0 Basic CRUD/04. insert_data_into_employees_table.sql
@@ -1,2 +1,9 @@
 -- Insert data Into Employees Table
 
+INSERT INTO employees 
+	(first_name, last_name, job_title, department_id, salary)
+VALUES
+	('Samantha', 'Young', 'Housekeeping', 4, 900),
+	('Roger', 'Palmer', 'Waiter', 3, 928.33)
+;
+SELECT * FROM employees
