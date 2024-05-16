cb3dcae93fc30e46cf97a47723e81aa4036951cc
me_madness <p_rangel@abv.bg>
2024-05-16 20:06:53 +0200

    small changes in file 06

--- Python PostgreSQL/2.0 Basic CRUD/06. delete_from_table.sql
+++ Python PostgreSQL/2.0 Basic CRUD/06. delete_from_table.sql
@@ -4,6 +4,7 @@ DELETE FROM employees
 WHERE department_id = 2 or department_id = 1
 --WHERE department_id BETWEEN 1 and 2
 --WHERE department_id IN (1, 2)
+--WHERE department_id <= 2
 ;
 
 SELECT * FROM employees
