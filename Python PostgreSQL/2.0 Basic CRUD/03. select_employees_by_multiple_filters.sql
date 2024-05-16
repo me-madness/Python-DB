3a58e4579e20ac7a8a89fd4f3eaf0a5c774c66bd
me_madness <p_rangel@abv.bg>
2024-05-16 19:11:42 +0200

    task 03 is done

--- Python PostgreSQL/2.0 Basic CRUD/03. select_employees_by_multiple_filters.sql
+++ Python PostgreSQL/2.0 Basic CRUD/03. select_employees_by_multiple_filters.sql
@@ -1,2 +1,12 @@
 -- Select Employees by Multiple Filters
 
+SELECT
+	id,
+	first_name,
+	last_name,
+	job_title,
+	department_id,
+	salary
+FROM employees
+WHERE department_id=4 and salary >= 1000
+;
