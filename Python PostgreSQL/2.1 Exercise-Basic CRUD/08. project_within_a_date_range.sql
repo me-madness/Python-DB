facc207ebc88cbc25749fc8ac094a15594b056a3
me_madness <p_rangel@abv.bg>
2024-05-17 19:06:40 +0200

    08 is done

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/08. project_within_a_date_range.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/08. project_within_a_date_range.sql
@@ -1,2 +1,14 @@
 -- Project within a Date Range
 
+SELECT 
+	name,
+	start_date,
+	end_date
+FROM 
+	projects
+WHERE 
+	start_date >= '2016-06-01 07:00:00'
+		AND
+	end_date < '2023-06-04 00:00:00'
+ORDER BY
+	start_date;
