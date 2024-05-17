a98eac988d879bd92975c9a57cffa61bda58bc3f
me_madness <p_rangel@abv.bg>
2024-05-17 18:26:05 +0200

    task 07 is done

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/07. positive_even_number.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/07. positive_even_number.sql
@@ -1,2 +1,19 @@
 -- Positive Even Number
 
+
+SELECT
+	id,
+	CONCAT(
+    number,    
+	' ',
+	street
+	) AS address,
+	city_id
+FROM
+	addresses
+WHERE
+	city_id > 0
+        AND
+    city_id % 2 = 0
+ORDER BY
+    city_id;
