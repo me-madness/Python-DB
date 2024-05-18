39dc6d3fcce8756e91b50651e4aba7a3ef864430
me_madness <p_rangel@abv.bg>
2024-05-18 20:48:55 +0200

    task 15 is ready

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/15. new_table.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/15. new_table.sql
@@ -1,2 +1,15 @@
 -- New Table
 
+CREATE TABLE IF NOT EXISTS company_chart
+AS 
+SELECT
+	CONCAT(
+		first_name,
+		' ',
+		last_name
+	) AS full_name,
+	job_title,	
+	department_id,
+	manager_id
+FROM
+	employees;
