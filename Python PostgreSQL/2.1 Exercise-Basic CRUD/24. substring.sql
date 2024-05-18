af1fbaec35f1dd562c427cf6d5ef2ee49e6e5f41
me_madness <p_rangel@abv.bg>
2024-05-19 01:22:54 +0200

    task 24 is done

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/24. substring.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/24. substring.sql
@@ -1,2 +1,14 @@
 -- Substring
 
+CREATE VIEW
+	view_initials
+AS
+SELECT
+	SUBSTRING(first_name, 1, 2) AS initial,
+	last_name
+FROM
+	employees
+ORDER BY
+	last_name;
+
+SELECT* FROM view_initials;
