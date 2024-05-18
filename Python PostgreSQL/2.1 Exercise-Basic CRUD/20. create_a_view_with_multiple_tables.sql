f1405c0b8a32101459af1b548641bdad6e865a89
me_madness <p_rangel@abv.bg>
2024-05-19 01:11:03 +0200

    task 20 is done

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/20. create_a_view_with_multiple_tables.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/20. create_a_view_with_multiple_tables.sql
@@ -1,2 +1,19 @@
 -- Create a View with Multiple tables
 
+	CONCAT(
+		e.first_name,
+		' ',
+		e.last_name
+	) AS full_name,
+	e.department_id,
+	CONCAT(
+	a.number,
+	' ',
+	a.street
+	)	AS address
+FROM
+	employees AS e, addresses AS a
+WHERE 
+	a.id = e.address_id
+ORDER BY
+	address;
