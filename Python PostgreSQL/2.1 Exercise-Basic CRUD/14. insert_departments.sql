93342792fc13568f641dd41cd690e76dd190f352
me_madness <p_rangel@abv.bg>
2024-05-18 20:43:09 +0200

    task 14 is done

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/14. insert_departments.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/14. insert_departments.sql
@@ -1,2 +1,13 @@
 -- Insert Departments
 
+INSERT INTO
+	departments(department, manager_id)
+VALUES
+	('Finance', 3),
+	('Information Services', 42),
+	('Document Control', 90),
+	('Quality Assurance', 274),
+	('Facilities and Maintenance', 218),
+	('Shipping and Receiving', 85),
+	('Executive', 109)
+RETURNING *;
