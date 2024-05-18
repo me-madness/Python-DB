8c7d5aeb019a90e51101ea8a6c707adca9523e5e
me_madness <p_rangel@abv.bg>
2024-05-18 21:16:08 +0200

    task 19 is done

--- Python PostgreSQL/2.1 Exercise-Basic CRUD/19. create_a_view.sql
+++ Python PostgreSQL/2.1 Exercise-Basic CRUD/19. create_a_view.sql
@@ -1,2 +1,16 @@
 -- Create a View
 
+CREATE VIEW
+	view_company_chart
+AS
+SELECT
+	CONCAT(
+	first_name,
+	' ',
+	last_name
+	) AS full_name,
+	job_title
+FROM
+	employees
+WHERE
+	manager_id = 184;
