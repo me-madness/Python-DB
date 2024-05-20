fe0a7cd37de295e3f79dfdecbb6862114d33a82e
me_madness <p_rangel@abv.bg>
2024-05-20 22:57:36 +0200

    new commit in my file

--- Python PostgreSQL/main.py
+++ Python PostgreSQL/main.py
@@ -10,13 +10,11 @@ def is_leap(year):
     if year % 4 == 0:
         leap = True
         if year % 100 == 0:
+            leap = False
+        elif year % 400 == 0:
             leap = True 
-            if year % 400 == 0:
-                leap = True 
-            else:
-                leap = False
         else:
-            leap = False                
+            leap = False               
     else:
         leap = False
     return leap
