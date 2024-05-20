5d6c8f13bca3459d39860a1f4e1a19d83c0b5933
me_madness <p_rangel@abv.bg>
2024-05-20 22:47:25 +0200

    new task from hackerank

--- Python PostgreSQL/main.py
+++ Python PostgreSQL/main.py
@@ -1,5 +1,25 @@
-if __name__ == '__main__':
-    n = int(input())
-for i in range(n):
-     name = i * i
-     print(name)   
+# if __name__ == '__main__':
+#     n = int(input())
+# for i in range(n):
+#      name = i * i
+#      print(name)   
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        leap = True
+        if year % 100 == 0:
+            leap = True 
+            if year % 400 == 0:
+                leap = True 
+            else:
+                leap = False
+        else:
+            leap = False                
+    else:
+        leap = False
+    return leap
+
+year = int(input())
+print(is_leap(year))
