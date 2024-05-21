033c589d6c0bae0b82ec085040aad6846716a112
me_madness <p_rangel@abv.bg>
2024-05-21 20:08:29 +0200

    task 13 and 14 are done

--- Python PostgreSQL/3.1 Exercise-Built-in Functions/13. positive_and_negative_left.sql
+++ Python PostgreSQL/3.1 Exercise-Built-in Functions/13. positive_and_negative_left.sql
@@ -1,2 +1,8 @@
 -- Positive and Negative LEFT
 
+SELECT
+	peak_name,
+	LEFT(peak_name, 4) AS positive_left, -- Get the first 4
+	LEFT(peak_name, -4) AS negative_left -- Get everything but the last 4
+FROM
+	peaks;
--- Python PostgreSQL/3.1 Exercise-Built-in Functions/14. positive_nad_negative_right.sql
+++ Python PostgreSQL/3.1 Exercise-Built-in Functions/14. positive_nad_negative_right.sql
@@ -1,2 +1,8 @@
 -- Positive and Negative RIGHT
 
+SELECT
+	peak_name,
+	RIGHT(peak_name, 4) AS positive_right, -- Get the first 4
+	RIGHT(peak_name, -4) AS negative_right -- Get everything but the last 4
+FROM
+	peaks;
