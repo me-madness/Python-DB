1c2bfbb2bee7fc6c35964e128e48f76f1d813ef7
me_madness <p_rangel@abv.bg>
2024-05-21 21:03:56 +0200

    task 24 is done

--- Python PostgreSQL/3.1 Exercise-Built-in Functions/24. match_or_not.sql
+++ Python PostgreSQL/3.1 Exercise-Built-in Functions/24. match_or_not.sql
@@ -1,2 +1,11 @@
 -- Match or Not
 
+SELECT
+	companion_full_name,
+	email
+FROM 
+	users
+WHERE
+	companion_full_name ILIKE '%aNd%' -- and And aNd anD...
+		AND
+	email NOT LIKE '%@gmail'; -- sasho@gmail - not valid; sasho@gmail.com - valid cause its not ending gmail
