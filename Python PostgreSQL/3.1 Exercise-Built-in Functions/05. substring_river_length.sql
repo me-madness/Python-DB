5886489a2b08d46578feab84602f240314f29510
me_madness <p_rangel@abv.bg>
2024-05-21 19:05:41 +0200

    task 05 is done

--- Python PostgreSQL/3.1 Exercise-Built-in Functions/05. substring_river_length.sql
+++ Python PostgreSQL/3.1 Exercise-Built-in Functions/05. substring_river_length.sql
@@ -1,2 +1,13 @@
 -- Substring River Length
 
+-- First Way
+SELECT
+	(REGEXP_MATCHES("River Information", '[0-9]{1,4}'))[1] AS river_length
+FROM
+	view_river_info;
+
+-- Second Way
+SELECT
+	SUBSTRING("River Information", '[0-9]{1,4}') AS river_length
+FROM	
+	view_river_info;
