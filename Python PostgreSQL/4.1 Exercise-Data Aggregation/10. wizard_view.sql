cc40109035b1ffa5342c8b0a1006bac28cbd9e9d
me_madness <p_rangel@abv.bg>
2024-05-23 19:12:44 +0200

    task 10 is done now

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/10. wizard_view.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/10. wizard_view.sql
@@ -14,6 +14,6 @@ SELECT
 		deposit_amount AS amount		
 FROM
 	wizard_deposits
-WHERE deposit_expiration_date < '1983-08-17'	
+WHERE deposit_expiration_date <= '1983-08-17'	
 GROUP BY wizard_name, deposit_start_date, deposit_expiration_date, deposit_amount	
 ORDER BY expiration_date;
