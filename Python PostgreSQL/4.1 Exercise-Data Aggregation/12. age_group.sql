4d833f49355b90f8c3db87c1023b850fb5b9bc03
me_madness <p_rangel@abv.bg>
2024-05-23 19:30:22 +0200

    task 12 is done

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/12. age_group.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/12. age_group.sql
@@ -1,2 +1,17 @@
 -- Age Group
 
+SELECT
+	CASE 
+		WHEN age BETWEEN 0 and 10 THEN '[0-10]'
+		WHEN age BETWEEN 11 and 20 THEN '[11-20]'
+		WHEN age BETWEEN 21 and 30 THEN '[21-30]'
+		WHEN age BETWEEN 31 and 40 THEN '[31-40]'
+		WHEN age BETWEEN 41 and 50 THEN '[41-50]'
+		WHEN age BETWEEN 51 and 60 THEN '[51-60]'
+		ELSE '[61+]'
+	END AS age_group,
+	count(age)
+FROM
+	wizard_deposits
+GROUP BY age_group
+ORDER BY age_group;
