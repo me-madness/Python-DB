92f46ce1f636b2bf807809d6c9fc8397acb5923c
me_madness <p_rangel@abv.bg>
2024-05-23 17:28:05 +0200

    adding some changes in task 04

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/04. min_deposit_charge.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/04. min_deposit_charge.sql
@@ -1,6 +1,16 @@
 -- Min Deposit Charge
 
+-- FIrst Way
 SELECT
 	min(deposit_charge) AS minimum_deposit_charge
 FROM
 	wizard_deposits;
+
+-- Second Way
+SELECT 
+	deposit_charge AS minimum_deposit_charge
+FROM	
+	wizard_deposits
+ORDER BY
+	deposit_charge
+LIMIT 1;	
