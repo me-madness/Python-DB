ca4f7b4beebb9c9560f822aad12b22ae29fe8309
me_madness <p_rangel@abv.bg>
2024-05-23 17:30:29 +0200

    task 05 is done now

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/05. max_age.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/05. max_age.sql
@@ -1,6 +1,16 @@
 -- Max Age
 
+-- FIrst Way
 SELECT
 	max(age) AS maximum_age
 FROM
 	wizard_deposits;
+
+-- Second Way
+SELECT
+	age AS maximum_age
+FROM
+	wizard_deposits
+ORDER BY
+	age DESC
+LIMIT 1;    
