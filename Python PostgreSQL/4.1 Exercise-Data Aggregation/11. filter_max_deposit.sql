e8517068ee32a39f9aacbcb90caf9e7ec3b904a8
me_madness <p_rangel@abv.bg>
2024-05-23 19:19:32 +0200

    task 11 is done

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/11. filter_max_deposit.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/11. filter_max_deposit.sql
@@ -1,2 +1,11 @@
 -- Filter max deposit
 
+SELECT
+	magic_wand_creator,
+	max(deposit_amount) AS max_deposit_amount
+FROM
+	wizard_deposits
+GROUP BY magic_wand_creator
+HAVING max(deposit_amount) NOT BETWEeN 20000 and 40000
+ORDER BY max_deposit_amount DESC	
+LIMIT 3;
