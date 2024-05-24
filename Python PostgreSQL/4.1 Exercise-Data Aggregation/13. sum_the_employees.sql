d729ac8e4f0bbb02f10db4094a368292fc87e1e1
me_madness <p_rangel@abv.bg>
2024-05-24 21:19:29 +0200

    task 13 is done

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/13. sum_the_employees.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/13. sum_the_employees.sql
@@ -1,2 +1,11 @@
 -- Sum the Employees
 
+SELECT 
+	sum(CASE department_id WHEN 1 THEN 1 ELSE 0 END) AS "Engineering",
+	sum(CASE department_id WHEN 2 THEN 1 ELSE 0 END) AS "Tool Design",
+	sum(CASE department_id WHEN 3 THEN 1 ELSE 0 END) AS "Sales",
+	sum(CASE department_id WHEN 4 THEN 1 ELSE 0 END) AS "Marketing",
+	sum(CASE department_id WHEN 5 THEN 1 ELSE 0 END) AS "Purchasing",
+	sum(CASE department_id WHEN 6 THEN 1 ELSE 0 END) AS "Research and Development",
+	sum(CASE department_id WHEN 7 THEN 1 ELSE 0 END) AS "Production"
+FROM employees;
