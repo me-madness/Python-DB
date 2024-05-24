0369f2579077e9ed3e49c28c4b3cafe448377dc2
me_madness <p_rangel@abv.bg>
2024-05-24 21:23:48 +0200

    task 14 is done

--- Python PostgreSQL/4.1 Exercise-Data Aggregation/14. update_employees_data.sql
+++ Python PostgreSQL/4.1 Exercise-Data Aggregation/14. update_employees_data.sql
@@ -1,2 +1,15 @@
 -- Update Employees Data
 
+UPDATE employees
+SET salary =
+	CASE
+		WHEN hire_date < '2015-01-16' THEN salary + 2500
+		WHEN hire_date < '2020-03-04' THEN salary + 1500
+		ELSE salary
+	END,
+job_title =
+	CASE
+		WHEN hire_date < '2015-01-16' THEN concat('Senior', job_title)
+		WHEN hire_date < '2020-03-04' THEN cincat('Mid-', job_title)
+		ELSE salary
+	END;
