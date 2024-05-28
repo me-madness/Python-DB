5dd49e36c696dbcbceef9822d0217142b9c3fbbe
me_madness <p_rangel@abv.bg>
2024-05-28 20:02:44 +0200

    Task 14 is done

--- Python PostgreSQL/5.1 Exercise-Table Relations/14. peaks_in_rila.sql
+++ Python PostgreSQL/5.1 Exercise-Table Relations/14. peaks_in_rila.sql
@@ -1,2 +1,16 @@
 -- Peaks in Rila
 
+SELECT
+	mountain_range,
+	peak_name,
+	elevation
+FROM
+	peaks
+JOIN
+	mountains
+ON
+	peaks.mountain_id = mountains.id
+WHERE
+	mountain_range LIKE '%Rila%'
+ORDER BY
+	elevation DESC;
