0470e636136a8bcac2b91d4e22d454f7b5c41dee
me_madness <p_rangel@abv.bg>
2024-05-28 19:02:27 +0200

    task 08 is all most done

--- Python PostgreSQL/5.1 Exercise-Table Relations/08. study_session.sql
+++ Python PostgreSQL/5.1 Exercise-Table Relations/08. study_session.sql
@@ -1,2 +1,37 @@
 -- Study Session
 
+CREATE TABLE students(
+	id SERIAL PRIMARY KEY,
+	student_name VARCHAR(50)
+);
+
+CREATE TABLE exams(
+	id INT GENERATED ALWAYS AS IDENTITY (START WITH 101 INCREMENT 1) PRIMARY KEY,
+	exam_name VARCHAR(50)
+);
+
+CREATE TABLE study_halls(
+	id SERIAL PRIMARY KEY
+	study_hall_name VARCHAR(50,
+	exam_id INT,
+
+	CONSTRAINT fk_study_halls_exams
+	FOREIGN KEY (exam_id)
+	REFERENCES exams(id)
+);
+
+CREATE TABLE students_exams(
+	student_id INT,
+	exam_id INT,
+
+	CONSTRAINT pk_students_exams
+	PRIMARY KEY (student_id, exam_id),
+
+	CONSTRAINT fk_students_exams_students
+	FOREIGN KEY (student_id)
+	REFERENCES students(id),
+
+	CONSTRAINT fk_students_exams_exams
+	FOREIGN KEY (exam_id)
+	REFERENCES exams(id)
+);
