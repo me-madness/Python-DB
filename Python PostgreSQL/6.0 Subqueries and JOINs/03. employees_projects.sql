f24b4d0a475d283d793bdf6ee29ab7fbc809ec21
me_madness <p_rangel@abv.bg>
2024-05-30 19:38:01 +0200

    Task 03 is done by two ways

--- Python PostgreSQL/6.0 Subqueries and JOINs/03. employees_projects.sql
+++ Python PostgreSQL/6.0 Subqueries and JOINs/03. employees_projects.sql
@@ -1,2 +1,16 @@
 -- Employees Project
 
+SELECT 
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) as full_name,
+	p.project_id,
+	p.name as project_name
+FROM
+	employees as e
+		JOIN employees_projects as ep
+			USING (employee_id)
+				JOIN projects as p
+					ON ep.project_id = p.project_id
+                    -- USING (project_id) Second option
+WHERE p.project_id = 1;
+				
