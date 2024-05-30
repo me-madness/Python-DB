63db301277613fb6788bea93c300481ba3be40d2
me_madness <p_rangel@abv.bg>
2024-05-30 18:47:15 +0200

    begin with task 01

--- Python PostgreSQL/6.0 Subqueries and JOINs/01. towns_addresses.sql
+++ Python PostgreSQL/6.0 Subqueries and JOINs/01. towns_addresses.sql
@@ -1,2 +1,11 @@
 -- Towns Addresses
 
+SELECT
+	t.town_id,
+	t.name as town_name,
+	a.address_text
+FROM
+	towns as t
+		JOIN addresses as a
+			ON t.town_id = a.address_id
+ORDER BY t.town_id, a.address_id;
