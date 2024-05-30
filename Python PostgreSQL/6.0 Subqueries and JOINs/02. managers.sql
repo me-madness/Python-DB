3f4a183a7ee2ffc1e240a97d4f12dfaf9d010b75
me_madness <p_rangel@abv.bg>
2024-05-30 18:56:17 +0200

    Task 02 is done

--- Python PostgreSQL/6.0 Subqueries and JOINs/02. managers.sql
+++ Python PostgreSQL/6.0 Subqueries and JOINs/02. managers.sql
@@ -1,2 +1,15 @@
 -- Managers
 
+SELECT 
+	e.employee_id,
+	CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+	d.department_id,
+	d.name
+FROM 
+	employees as e 
+		JOIN
+	departments as d
+		ON 
+	e.employee_id = d.manager_id
+ORDER BY e.employee_id
+LIMIT 5;
