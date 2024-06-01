13b1c058a8db9e1e40adf35253d7b49b2c8e65db
me_madness <p_rangel@abv.bg>
2024-06-01 17:03:38 +0200

    Task 13 is done

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/13. rivers_in_africa.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/13. rivers_in_africa.sql
@@ -1,2 +1,20 @@
 -- Rivers in Africa
 
+SELECT
+	c.country_name,
+	r.river_name
+FROM
+	countries As c
+LEFT JOIN
+	countries_rivers AS cr
+USING
+	(country_code)
+LEFT JOIN
+	rivers AS r
+ON 
+	r.id = cr.river_id
+WHERE
+	c.continent_code = 'AF'
+ORDER BY
+	c.country_name
+LIMIT 5;
