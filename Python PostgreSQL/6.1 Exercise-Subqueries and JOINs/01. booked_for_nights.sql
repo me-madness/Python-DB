857ff45cbbdec6d57ce98443a0eefc30e829959e
me_madness <p_rangel@abv.bg>
2024-06-01 14:59:52 +0200

    Task 01 from lesson 6.1 Exercise - Subqueries and JOIN's

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/01. booked_for_nights.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/01. booked_for_nights.sql
@@ -1,2 +1,27 @@
 -- Booked for Nights
 
+-- First Option
+SELECT
+	a.address || ' ' || a.address_2 AS apartment_address,
+	b.booked_for AS nights
+FROM
+	apartments AS a
+JOIN
+	bookings as b
+USING
+	(booking_id)
+ORDER BY 
+	a.apartment_id ASC;
+
+-- Second Option
+SELECT
+	a.address || ' ' || a.address_2 AS apartment_address,
+	b.booked_for AS nights
+FROM
+	apartments AS a
+JOIN
+	bookings as b
+ON 
+    a.booking_id = b.booking_id 
+ORDER BY 
+	a.apartment_id ASC;    
