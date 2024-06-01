38681c5f63ba8ddefff4202294a9dce2601ebdf5
me_madness <p_rangel@abv.bg>
2024-06-01 17:08:47 +0200

    Task 14 is done

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/14. minimum_average_area_across_continents.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/14. minimum_average_area_across_continents.sql
@@ -1,2 +1,13 @@
 -- Minimum Average Area Across Continents
 
+SELECT 
+	MIN(average_area) AS min_average_area
+FROM(
+	SELECT
+		continent_code,
+		AVG(area_in_sq_km) AS average_area
+	FROM
+		countries
+	GROUP BY
+		continent_code
+) AS min_average_area;
