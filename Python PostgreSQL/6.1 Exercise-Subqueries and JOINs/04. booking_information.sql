686f955f7e9d9432f5144e4ad8467c266d338779
me_madness <p_rangel@abv.bg>
2024-06-01 15:25:01 +0200

    adding a second option for task 04

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/04. booking_information.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/04. booking_information.sql
@@ -1,5 +1,6 @@
 -- Booking Information
 
+-- First Option
 SELECT
 	b.booking_id,
 	a.name AS apartment_owner,
@@ -9,6 +10,27 @@ SELECT
 		' ',
 		c.last_name
 	) AS customer_name
+FROM
+	bookings AS b
+FULL JOIN
+	apartments AS a
+USING 
+	(booking_id)
+FULL JOIN
+	customers AS c
+USING
+	(customer_id)
+ORDER BY
+	b.booking_id,
+	apartment_owner,
+	customer_name;
+
+-- Second Option
+SELECT
+	b.booking_id,
+	a.name AS apartment_owner,
+	a.apartment_id,
+	c.first_name || ' ' || c.last_name AS customer_name
 FROM
 	bookings AS b
 FULL JOIN
