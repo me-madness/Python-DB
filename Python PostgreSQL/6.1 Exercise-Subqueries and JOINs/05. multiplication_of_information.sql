b80844b90236d61097a42e8cfc0cf18c8a523bde
me_madness <p_rangel@abv.bg>
2024-06-01 15:32:07 +0200

    task 05 is done

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/05. multiplication_of_information.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/05. multiplication_of_information.sql
@@ -1,2 +1,20 @@
 -- Multiplication of Information
 
+-- First Option
+SELECT
+	b.booking_id,
+	c.first_name AS customer_name
+FROM
+	bookings AS b,
+	customers AS c;
+
+-- Second Option
+SELECT
+	b.booking_id,
+	c.first_name AS customer_name
+FROM
+	bookings AS b
+CROSS JOIN
+    customers AS c
+ORDER BY 
+    customer_name;    
