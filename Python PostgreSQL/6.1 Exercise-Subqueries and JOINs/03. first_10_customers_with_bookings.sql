973083e460d02c38f4055c4ee33dfcec25d9ef45
me_madness <p_rangel@abv.bg>
2024-06-01 15:13:35 +0200

    Task 03 is done

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/03. first_10_customers_with_bookings.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/03. first_10_customers_with_bookings.sql
@@ -1,2 +1,20 @@
 -- First 10 Customers with Bookings
 
+SELECT
+	b.booking_id,
+	b.starts_at :: DATE,
+	b.apartment_id,
+	CONCAT(
+		c.first_name,
+		' ',
+		c.last_name
+	) AS customer_name
+FROM
+	bookings AS b
+RIGHT JOIN
+	customers AS c
+USING 
+	(customer_id)
+ORDER BY
+	customer_name
+LIMIT 10;
