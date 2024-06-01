470c3cfe3174739ebd827ab63d9bad58021c15dc
me_madness <p_rangel@abv.bg>
2024-06-01 16:39:59 +0200

    Task 12 is done

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/12. count_mountain_ranges.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/12. count_mountain_ranges.sql
@@ -1,2 +1,17 @@
 -- Count Mountain ranges
 
+SELECT
+	mc.country_code,
+	COUNT(m.mountain_range) AS mountain_rage_count
+FROM
+	mountains_countries AS mc
+JOIN
+	mountains AS m
+ON
+	m.id = mc.mountain_id
+WHERE 
+	mc.country_code IN ('US', 'RU', 'BG')
+GROUP BY
+	mc.country_code
+ORDER BY
+	mountain_rage_count DESC;
