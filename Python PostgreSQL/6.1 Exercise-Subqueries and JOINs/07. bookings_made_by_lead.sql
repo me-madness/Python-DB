ca17092ea9e86b4cd45c7dbf0540813f422e3145
me_madness <p_rangel@abv.bg>
2024-06-01 15:49:23 +0200

    Task 07 is done

--- Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/07. bookings_made_by_lead.sql
+++ Python PostgreSQL/6.1 Exercise-Subqueries and JOINs/07. bookings_made_by_lead.sql
@@ -1,2 +1,15 @@
 -- Bookings Made by Lead
 
+SELECT
+	b.apartment_id,
+	b.booked_for,
+	c.first_name,
+	c.country
+FROM
+	bookings AS b
+JOIN
+	customers AS c
+USING
+	(customer_id)
+WHERE
+	c.job_type = 'Lead';
