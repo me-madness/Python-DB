a326f3c3f4df4d81ec4ea0c2f35b2ef51fefbcb6
me_madness <p_rangel@abv.bg>
2024-06-06 20:44:46 +0200

    One more option is added in task 11

--- Python PostgreSQL/Exam Preparation - 06.06.2024/11. all_volunteers_in_a_department.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/11. all_volunteers_in_a_department.sql
@@ -39,3 +39,24 @@ $$
 	END;	
 $$	
 LANGUAGE plpgsql;
+
+-- Third Option
+CREATE OR REPLACE FUNCTION fn_get_volunteers_count_from_department(searched_volunteers_department VARCHAR(30))
+RETURNS INT AS
+$$
+	DECLARE
+		volunteer INT;
+	BEGIN
+		volunteer :=
+			(SELECT 
+				count(*)
+			FROM 
+				volunteers AS v
+					JOIN volunteers_departments AS vd
+						ON vd.id = v.department_id
+			WHERE vd.department_name = searched_volunteers_department
+		);
+		RETURN volunteer;
+	END;	
+$$	
+LANGUAGE plpgsql;
