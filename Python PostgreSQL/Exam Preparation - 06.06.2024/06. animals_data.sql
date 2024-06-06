a1bd5573a7f057dd03ba34726954ded30cd31549
me_madness <p_rangel@abv.bg>
2024-06-06 19:35:15 +0200

    Task 06 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/06. animals_data.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/06. animals_data.sql
@@ -1,2 +1,10 @@
 -- Animals Data
 
+SELECT
+	a.name,
+	at.animal_type,
+	to_char(a.birthdate, 'DD.MM.YYYY') AS birthdate
+FROM animals AS a
+	JOIN animal_types AS at
+		ON a.animal_type_id = at.id
+ORDER BY name;
