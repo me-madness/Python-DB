0628375fa326608d7b6a7553144734ff174c9fe9
me_madness <p_rangel@abv.bg>
2024-06-06 19:39:47 +0200

    Task 07 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/07. owners_and_their_animals.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/07. owners_and_their_animals.sql
@@ -1,2 +1,12 @@
 -- Owners and Their Animals
 
+SELECT
+	o.name AS owner,
+	count(*) AS count_of_animals
+FROM
+	owners AS o
+		JOIN animals AS a
+			ON a.owner_id = o.id
+GROUP BY owner
+ORDER BY count_of_animals DESC, owner
+LIMIT 5;
