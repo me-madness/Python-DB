e18930b9dfb7ea5b3c934b48f5e87222019e0337
me_madness <p_rangel@abv.bg>
2024-06-06 19:51:36 +0200

    Task 08 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/08. owners_animals_and_cages.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/08. owners_animals_and_cages.sql
@@ -1,2 +1,16 @@
 -- Owners, Animals and Cages
 
+SELECT
+	CONCAT(o.name, ' - ', a.name) AS "owners - animals",
+	o.phone_number,
+	ac.cage_id
+FROM
+	owners AS o
+		JOIN animals AS a
+			ON o.id = a.owner_id
+				JOIN animals_cages AS ac
+					ON ac.animal_id = a.id
+						JOIN animal_types AS at
+							ON a.animal_type_id = at.id
+WHERE at.animal_type = 'Mammals'	
+ORDER BY o.name, a.name DESC;
