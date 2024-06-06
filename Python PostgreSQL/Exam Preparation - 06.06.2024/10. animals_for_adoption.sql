740604b53debb10c22bd29d305abec0c43fc4a56
me_madness <p_rangel@abv.bg>
2024-06-06 20:12:02 +0200

    Task 10 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/10. animals_for_adoption.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/10. animals_for_adoption.sql
@@ -1,2 +1,15 @@
 -- Animals for Adoption
 
+SELECT
+	a.name AS animal,
+	EXTRACT('year' FROM birthdate) AS birth_year,
+	at.animal_type
+FROM
+	animals AS a
+		JOIN animal_types AS at
+			ON a.animal_type_id = at.id
+WHERE 
+	at.animal_type <> 'Birds'
+	and age('01/01/2022', a.birthdate) < '5 years'
+	and a.owner_id IS NULL
+ORDER BY a.name;
