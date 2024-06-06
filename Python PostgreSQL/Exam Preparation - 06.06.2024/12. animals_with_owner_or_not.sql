367e6ca6f21e99b018aecd04e1d9c3267be74048
me_madness <p_rangel@abv.bg>
2024-06-06 21:00:15 +0200

    Task 12 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/12. animals_with_owner_or_not.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/12. animals_with_owner_or_not.sql
@@ -1,2 +1,22 @@
 -- Animals with Owners or not
 
+CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not(
+	IN animal_name VARCHAR(30), 
+	OUT result VARCHAR(30)
+)
+AS
+$$
+	BEGIN
+		SELECT
+			o.name
+		INTO result
+		FROM
+			owners AS o
+				JOIN animals AS a
+					ON o.id = a.owner_id
+		WHERE a.name = animal_name;
+
+		if result is NULL then result := 'For adoption'; end if;
+	END;
+$$
+LANGUAGE plpgsql;
