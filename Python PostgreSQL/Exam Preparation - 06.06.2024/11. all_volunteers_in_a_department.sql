3029630687511f1d5bc88530888f78f255aef6a4
me_madness <p_rangel@abv.bg>
2024-06-06 20:26:09 +0200

    Task 11 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/11. all_volunteers_in_a_department.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/11. all_volunteers_in_a_department.sql
@@ -1,2 +1,20 @@
 -- All Volunteers in a Department
 
+CREATE OR REPLACE FUNCTION fn_get_volunteers_count_from_department(searched_volunteers_department VARCHAR(30))
+RETURNS INT AS
+$$
+	DECLARE
+		volunteer INT;
+	BEGIN
+		SELECT 
+			count(*)
+		INTO volunteer
+		FROM 
+			volunteers AS v
+				JOIN volunteers_departments AS vd
+					ON vd.id = v.department_id
+		WHERE vd.department_name = searched_volunteers_department;
+		RETURN volunteer;
+	END;	
+$$	
+LANGUAGE plpgsql;
