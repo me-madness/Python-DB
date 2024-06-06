25e5946794c4c853ed0b13bc966219992be050a2
me_madness <p_rangel@abv.bg>
2024-06-06 20:01:21 +0200

    I add Second option for task 09

--- Python PostgreSQL/Exam Preparation - 06.06.2024/09. volunteers_in_sofia.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/09. volunteers_in_sofia.sql
@@ -1,9 +1,25 @@
 -- Volunteers in Sofia
 
+-- First Option
 SELECT
 	v.name AS volunteers,
 	v.phone_number,
 	substring(trim(replace(v.address, 'Sofia', '')), 3) AS address
+FROM
+	volunteers AS v
+		JOIN volunteers_departments AS vd
+			ON v.department_id = vd.id
+WHERE 
+	vd.department_name = 'Education program assistant'
+		AND 
+	v.address LIKE '%Sofia%'
+ORDER BY volunteers;
+
+-- Second Option
+SELECT
+	v.name AS volunteers,
+	v.phone_number,
+	trim(v.address, 'Sofia, ') AS address
 FROM
 	volunteers AS v
 		JOIN volunteers_departments AS vd
