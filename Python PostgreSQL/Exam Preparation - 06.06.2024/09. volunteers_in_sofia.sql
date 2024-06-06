b4a86986991d15db093912618a28ed550cdee258
me_madness <p_rangel@abv.bg>
2024-06-06 19:59:20 +0200

    Task 09 is done

--- Python PostgreSQL/Exam Preparation - 06.06.2024/09. volunteers_in_sofia.sql
+++ Python PostgreSQL/Exam Preparation - 06.06.2024/09. volunteers_in_sofia.sql
@@ -1,2 +1,15 @@
 -- Volunteers in Sofia
 
+SELECT
+	v.name AS volunteers,
+	v.phone_number,
+	substring(trim(replace(v.address, 'Sofia', '')), 3) AS address
+FROM
+	volunteers AS v
+		JOIN volunteers_departments AS vd
+			ON v.department_id = vd.id
+WHERE 
+	vd.department_name = 'Education program assistant'
+		AND 
+	v.address LIKE '%Sofia%'
+ORDER BY volunteers;
