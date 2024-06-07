b01ebcc64f4729b6e8256a2c6311db760e95aba8
me_madness <p_rangel@abv.bg>
2024-06-07 20:46:18 +0200

    Task 06 is done

--- Python PostgreSQL/Exam Preparation - 07.06.2024/06. drivers_and_cars.sql
+++ Python PostgreSQL/Exam Preparation - 07.06.2024/06. drivers_and_cars.sql
@@ -1,2 +1,23 @@
 -- Drivers and Cars
 
+SELECT
+	d.first_name,
+	d.last_name,
+	c.make,
+	c.model,
+	c.mileage
+FROM
+	drivers AS d
+JOIN
+	cars_drivers AS cd
+ON
+	d.id = cd.driver_id
+JOIN
+	cars AS c
+ON
+	c.id = cd.car_id
+WHERE 
+	c.mileage IS NOT NULL
+ORDER bY
+	c.mileage DESC,
+	d.first_name ASC;
