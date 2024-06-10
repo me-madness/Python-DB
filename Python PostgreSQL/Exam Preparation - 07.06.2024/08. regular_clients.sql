b2572c5c9a2701e3bec88686d1b19450420c8767
me_madness <p_rangel@abv.bg>
2024-06-10 20:18:54 +0200

    task 08 is almost done

--- Python PostgreSQL/Exam Preparation - 07.06.2024/08. regular_clients.sql
+++ Python PostgreSQL/Exam Preparation - 07.06.2024/08. regular_clients.sql
@@ -1,3 +1,12 @@
 -- Regular Clients
 
-SELECT * FROM
+SELECT 
+    *
+FROM
+    Clients AS cl
+JOIN
+    courses AS co
+ON
+    cl.id = co.client_id        
+WHERE
+    SUBSTRING(cl.full_name, 2, 1) = 'a'    
