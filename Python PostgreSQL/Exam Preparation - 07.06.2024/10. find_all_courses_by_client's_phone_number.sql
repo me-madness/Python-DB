c7a8158f85baa18c1c05aa5ccb125e630849054d
me_madness <p_rangel@abv.bg>
2024-06-11 20:21:12 +0200

    Task 10 is done

--- Python PostgreSQL/Exam Preparation - 07.06.2024/10. find_all_courses_by_client's_phone_number.sql
+++ Python PostgreSQL/Exam Preparation - 07.06.2024/10. find_all_courses_by_client's_phone_number.sql
@@ -1,3 +1,23 @@
 -- Find all Courses by Client’s Phone Number
 
-SELECT * FROM
+CREATE OR REPLACE FUNCTION fn_courses_by_client(
+	phone_number VARCHAR(20)
+) RETURN INT
+AS
+$$
+BEGIN
+	RETURN (
+		SELECT
+			COUNT(*)
+		FROM
+			clients AS cl
+		JOIN
+			courses AS co
+		ON
+			co.client_id = cl.id
+		WHERE
+			cl.phone_number = '(803 6386812'
+	);
+END;
+$$
+LANGUAGE plpgsql;
