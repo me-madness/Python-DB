2a179937324b299759c8928de1decf2ce28c1c93
me_madness <p_rangel@abv.bg>
2024-06-11 20:00:42 +0200

    Begin with task 09

--- Python PostgreSQL/Exam Preparation - 07.06.2024/09. full_information_of_courses.sql
+++ Python PostgreSQL/Exam Preparation - 07.06.2024/09. full_information_of_courses.sql
@@ -1,3 +1,16 @@
 -- Full Information of Courses
 
-SELECT * FROM
+SELECT 
+
+FROM
+    courses AS c
+JOIN
+    clients AS cl
+ON 
+    c.client_id - cl.id
+JOIN
+    cars AS cr
+ON
+    cr.id = car_id
+JOIN
+    ca                    
