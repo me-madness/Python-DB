e06e8b39ceebde2a7346160b9e248f9dfff7d232
me_madness <p_rangel@abv.bg>
2024-06-11 20:05:59 +0200

    Task 09 is done

--- Python PostgreSQL/Exam Preparation - 07.06.2024/09. full_information_of_courses.sql
+++ Python PostgreSQL/Exam Preparation - 07.06.2024/09. full_information_of_courses.sql
@@ -1,16 +1,33 @@
 -- Full Information of Courses
 
 SELECT 
-
+	a.name,
+	CASE
+		WHEN EXTRACT(HOUR FROM c.start) BETWEEN 6 AND 20 THEN 'Day'
+		ELSE 'Night'
+	END AS day_time,
+	c.bill,
+	cl.full_name,
+	cr.make,
+	cr.model,
+	ca.name
 FROM
     courses AS c
 JOIN
     clients AS cl
 ON 
-    c.client_id - cl.id
+    c.client_id = cl.id
 JOIN
     cars AS cr
 ON
     cr.id = car_id
 JOIN
-    ca                    
+    categories AS ca
+ON 
+	ca.id = cr.category_id
+JOIN
+	addresses AS a
+ON
+	a.id = c.from_address_id
+ORDER BY
+	c.id;                 
