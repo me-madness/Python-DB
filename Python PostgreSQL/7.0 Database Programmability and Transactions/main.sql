8a90df69e39e20b383b8a41b45fb113dac42826a
me_madness <p_rangel@abv.bg>
2024-06-12 20:36:30 +0200

    Task 02 is done iin main file

--- Python PostgreSQL/7.0 Database Programmability and Transactions/main.sql
+++ Python PostgreSQL/7.0 Database Programmability and Transactions/main.sql
@@ -11,9 +11,29 @@ $$
 $$
 LANGUAGE plpgsql;
 
-SELECT * FROM fn_full_name('Cvetan', 'Tomov')
+SELECT * FROM fn_full_name('Cvetan', 'Tomov');
 
 -- TAsk 02
+
+CREATE OR REPLACE FUNCTION fn_full_name(
+	VARCHAR, VARCHAR
+)
+RETURNS VARCHAR AS 
+$$
+	DECLARE
+		first_name ALIAS FOR $1;
+		last_name ALIAS FOR $2;
+		greeting VARCHAr;
+	BEGIN
+		greeting := 'Hello'
+		RETURN CONCAT(first_name, ' ', last_name)
+	END;
+$$
+LANGUAGE plpgsql;
+
+SELECT * FROM fn_full_name('Milko', 'Toshev')
+
+-- Task 03
 
 CREATE OR REPLACE FUNCTION (
 	
@@ -26,6 +46,15 @@ $$
 $$
 LANGUAGE plpgsql;
 
--- Task 03
+-- Task 04
 
-
+CREATE OR REPLACE FUNCTION (
+	
+)
+RETURNS VARCHAR AS 
+$$
+	BEGIN
+		
+	END;
+$$
+LANGUAGE plpgsql;
