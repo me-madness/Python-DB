efce91cb9cbb234f5797094a6dd36ac11f04a4fb
me_madness <p_rangel@abv.bg>
2024-06-12 20:48:22 +0200

    task 04 is done

--- Python PostgreSQL/7.0 Database Programmability and Transactions/main.sql
+++ Python PostgreSQL/7.0 Database Programmability and Transactions/main.sql
@@ -46,20 +46,33 @@ $$
 $$
 LANGUAGE plpgsql;
 
-SELECT * FROM fn_get_name_len(name: 'Koko');
+SELECT * FROM fn_get_name_len('Koko');
 
 -- Task 04
 
-CREATE OR REPLACE FUNCTION (
-	
+CREATE OR REPLACE FUNCTION fn_full_name(
+	first_name VARCHAR, last_name VARCHAR
 )
 RETURNS VARCHAR AS 
 $$
+	DECLARE
+		full_name VARCHAR(30)
 	BEGIN
-		
+		IF first_name is NULL AND last_name is NULL THEN
+			full_name := NULL;
+		ELSIF first_name is NULL THEN
+			full_name := last_name;
+		ELSIF last_name is NULL THEN
+			full_name := first_name;
+		ELSE
+			full_name := CONCAT(first_name, ' ', last_name);
+		END IF;
+		RETURN full_name;
 	END;
 $$
 LANGUAGE plpgsql;
+
+SELECT * FROM fn_full_name('Koko', NULL)
 
 -- Task 05
 
