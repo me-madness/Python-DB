4f32304aee534b0a9f14a027d75dfa4dd83478d5
me_madness <p_rangel@abv.bg>
2024-06-14 19:56:35 +0200

    Task 01 from Exercise: Database Programmability and Transactions

--- Python PostgreSQL/7.1 Exercise-Database Programmability and Transactions/01. user_defined_function_full_name.sql
+++ Python PostgreSQL/7.1 Exercise-Database Programmability and Transactions/01. user_defined_function_full_name.sql
@@ -1,2 +1,16 @@
 -- User-defined Function Full Name
 
+CREATE OR REPLACE FUNCTION fn_full_name (
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+) RETURNS VARCHAR(101)
+AS
+$$
+DECLARE 
+	full_name VARCHAR(101);
+BEGIN
+	full_name := INITCAP(LOWER(first_name)) || ' ' || INITCAP(LOWER(last_name));
+	RETURN full_name; -- full_name is not really needed
+END;
+$$
+LANGUAGE plpgsql;
