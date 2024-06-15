60d045c25ef6658fad18aa5b058f8613342c6148
me_madness <p_rangel@abv.bg>
2024-06-15 11:38:12 +0200

    Task 08 is done

---
+++ Python PostgreSQL/Regular Exam - 15.06.2024/08. counT_liked_and_comments.sql
@@ -0,0 +1,30 @@
+-- Count Likes and Comments
+
+SELECT
+    p.id AS photo_id,
+    COALESCE(l.likes_count, 0) AS likes_count,
+    COALESCE(c.comments_count, 0) AS comments_count
+FROM
+    photos p
+LEFT JOIN (
+    SELECT
+        photo_id,
+        COUNT(*) AS likes_count
+    FROM
+        likes
+    GROUP BY
+        photo_id
+) l ON p.id = l.photo_id
+LEFT JOIN (
+    SELECT
+        photo_id,
+        COUNT(*) AS comments_count
+    FROM
+        comments
+    GROUP BY
+        photo_id
+) c ON p.id = c.photo_id
+ORDER BY
+    likes_count DESC,
+    comments_count DESC,
+    photo_id ASC;
