8c1ef31c02c3764dd27a3c7687e4c87e3b2c9750
me_madness <p_rangel@abv.bg>
2024-06-16 17:03:21 +0200

    Task )9 is have Third option but is not finish

--- Python PostgreSQL/Regular Exam - 15.06.2024/09. photos_captured_on_tenth_day_of_the_month.sql
+++ Python PostgreSQL/Regular Exam - 15.06.2024/09. photos_captured_on_tenth_day_of_the_month.sql
@@ -1,5 +1,6 @@
+-- Photos Captured on the Tenth Day of the Month
 
-
+-- First Option
 SELECT
     CASE
         WHEN LENGTH(description) > 10 THEN SUBSTRING(description FROM 1 FOR 10) || '...'
@@ -26,3 +27,17 @@ WHERE
     EXTRACT(DAY FROM capture_date) = 10
 ORDER BY
     capture_date DESC;
+
+-- Third Option 
+SELECT
+    CASE
+        WHEN LENGTH(description) > 10 THEN LEFT(description, 10) || '...'
+        ELSE description
+    END AS summary,
+    TO_CHAR(capture_date, 'DD.MM HH24:MI') AS date
+FROM
+    photos
+WHERE
+    EXTRACT(DAY FROM capture_date) = 10
+ORDER BY
+    capture_date DESC;    
