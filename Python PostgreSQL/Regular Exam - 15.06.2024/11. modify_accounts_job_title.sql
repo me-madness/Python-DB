6589cdb4ee31bfa16cb3c290040e2c9b58641d8b
me_madness <p_rangel@abv.bg>
2024-06-16 16:59:54 +0200

    Task 11 is all most done

---
+++ Python PostgreSQL/Regular Exam - 15.06.2024/11. modify_accounts_job_title.sql
@@ -0,0 +1,13 @@
+-- Modify Accounts Job Title
+
+CREATE PROCEDURE udp_modify_account(
+    IN address_street VARCHAR(30),
+    IN address_town VARCHAR(30)
+)
+BEGIN
+    UPDATE accounts
+    SET job_title = CONCAT('(Remote) ', job_title)
+    WHERE address_street = address_street
+      AND address_town = address_town
+      AND job_title NOT LIKE '(Remote)%';
+END;
