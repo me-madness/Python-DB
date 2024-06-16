c1155a502c74d9e9daa3b928c8b516c639cd1b17
me_madness <p_rangel@abv.bg>
2024-06-16 17:01:20 +0200

    One more option for task 09

--- Python PostgreSQL/Regular Exam - 15.06.2024/09. photos_captured_on_tenth_day_of_the_month.sql
+++ Python PostgreSQL/Regular Exam - 15.06.2024/09. photos_captured_on_tenth_day_of_the_month.sql
@@ -12,3 +12,17 @@ WHERE
     EXTRACT(DAY FROM capture_date) = 10
 ORDER BY
     capture_date DESC;
+
+-- Second Option 
+SELECT
+    CASE
+        WHEN LENGTH(description) > 10 THEN DISTINCT(description FROM 1 FOR 10) || '...'
+        ELSE description
+    END AS summary,
+    TO_CHAR(capture_date, 'DD.MM HH24:MI') AS date
+FROM
+    photos
+WHERE
+    EXTRACT(DAY FROM capture_date) = 10
+ORDER BY
+    capture_date DESC;
