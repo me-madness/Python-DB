cc0af44d8954bafb25ed4e6368d903b3aff61ee0
me_madness <p_rangel@abv.bg>
2024-06-18 20:29:23 +0200

    small changes of first_blood file

--- Python ORM/00. ORM Introduction/Lab/first_blood.py
+++ Python ORM/00. ORM Introduction/Lab/first_blood.py
@@ -4,7 +4,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
+@app.get('/first_blood')
+def test_func():
+  return 'Hello Test'
 
 if __name__ == '__main__':
-  unicorn.run('test:app', host='127.0.0.1', port=4567, reload=True)
+  unicorn.run('first_blood:app', host='127.0.0.1', port=4557, reload=True)
