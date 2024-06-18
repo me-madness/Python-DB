bf8b2edc76e3f9a9eff4bf20cceddd7a00275e0d
me_madness <p_rangel@abv.bg>
2024-06-18 20:51:20 +0200

    adding a JSON file

--- Python ORM/00. ORM Introduction/Lab/first_blood.py
+++ Python ORM/00. ORM Introduction/Lab/first_blood.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 import unicorn
+import sqlite3
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
+
 @app.get('/first_blood')
 def test_func():
-  return 'Hello Test'
+  con = sqlite3.connect('name database')
 
 if __name__ == '__main__':
   unicorn.run('first_blood:app', host='127.0.0.1', port=4557, reload=True)
