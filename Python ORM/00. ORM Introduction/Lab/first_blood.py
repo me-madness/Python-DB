d31bf92ef29111616b91374367e43cf41e9e12b7
me_madness <p_rangel@abv.bg>
2024-06-18 21:04:08 +0200

    I need to ad a database

--- Python ORM/00. ORM Introduction/Lab/first_blood.py
+++ Python ORM/00. ORM Introduction/Lab/first_blood.py
@@ -6,12 +6,23 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class User(BaseModel):
-  username = str
-  password = str
+  username : str
+  password : str
 
 @app.get('/first_blood')
 def test_func():
   con = sqlite3.connect('name database')
+  query = f"SELECT * FROM users WHERE username = '{payload.username}' and password = '{payload.password}'"
+  print(query)
+  cur = con.cursor()
+  data = cur.execute(query).fetchhone()
+  if data is None:
+    return {'message': 'Invalid username or password'}
+  else:
+    return {
+      'user': data[0],
+      'password': data[1]
+    }
 
 if __name__ == '__main__':
   unicorn.run('first_blood:app', host='127.0.0.1', port=4557, reload=True)
