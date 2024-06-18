cce88375ac05807b849c8bede277daa5ea0905c9
me_madness <p_rangel@abv.bg>
2024-06-18 20:23:39 +0200

    new commit of my file first blood

--- Python ORM/00. ORM Introduction/Lab/first_blood.py
+++ Python ORM/00. ORM Introduction/Lab/first_blood.py
@@ -1,2 +1,10 @@
-class MyClass:
-  pass
+from fastapi import FastAPI
+import unicorn
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+
+if __name__ == '__main__':
+  unicorn.run('test:app', host='127.0.0.1', port=4567, reload=True)
