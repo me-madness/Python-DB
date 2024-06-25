352a96a06b34ca603492f82b9baead719e1e553b
me_madness <p_rangel@abv.bg>
2024-06-25 18:57:56 +0200

    task 03 Update emails is ready

--- Python ORM/05. Data Operations in Django with Queries/caller.py
+++ Python ORM/05. Data Operations in Django with Queries/caller.py
@@ -51,5 +51,14 @@ def get_students_info():
 
     return '\n'.join(result)
 
+
+def update_students_emails():
+    all_students = Student.objects.all()
+    for s in all_students:
+        s.email = s.email.replace(s.email.split('@')[1], 'uni-students.com')
+        s.save()
+
+    
+    
 # Run and print your queries
     
