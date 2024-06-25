f8f4720cd5c1693e266ad1a4543ae7b28e5e5453
me_madness <p_rangel@abv.bg>
2024-06-25 18:36:49 +0200

    i add some data for my database

--- Python ORM/05. Data Operations in Django with Queries/caller.py
+++ Python ORM/05. Data Operations in Django with Queries/caller.py
@@ -7,4 +7,38 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Student
+
+def  add_students():
+    student = Student(
+        student_id='FC5204',
+        first_name='John',
+        last_name='Doe',
+        birth_date='1995-05-15',
+        email='john.doe@university.com'
+    )
+    student.save()
+    
+    Student.objects.create(
+        student_id='FE0054',
+        first_name='Jane',
+        last_name='Smith',
+        email='jane.doe@university.com'
+    )
+    
+    student = Student()
+    student.student_id = 'FH2014'
+    student.first_name = 'Alice'
+    student.last_name = 'Johnson'
+    student.birth_date = '1998-02-10'
+    student.email = 'alice.johnson@university.com'
+    student.save()
+    
+    Student.objects.create(
+        student_id='FH2015',
+        first_name='Bob',
+        last_name='Wolson',
+        birth_date='1996-11-25',
+        email='bob.wolson@university.com'
+    )
 # Run and print your queries
