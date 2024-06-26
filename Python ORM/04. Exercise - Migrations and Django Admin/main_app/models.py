4d214a73fb78d5a29006504bc3a77653274eaf36
me_madness <p_rangel@abv.bg>
2024-06-26 21:15:12 +0200

    adding the new class event registration

--- Python ORM/04. Exercise - Migrations and Django Admin/main_app/models.py
+++ Python ORM/04. Exercise - Migrations and Django Admin/main_app/models.py
@@ -13,3 +13,18 @@ class UniqueBrands(models.Model):
             max_length=25,
             unique=True,
         )
+        
+        
+class EventRegistration(models.Model):
+    event_name = models.CharField(
+        max_length=60,
+    )
+
+    participant_name = models.CharField(
+        max_length=50,
+    )
+
+    registration_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.participant_name} - {self.event_name}"        
