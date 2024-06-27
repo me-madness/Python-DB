6b1b7f3f388df81eec37752a95d5f82c2cbbc8ae
me_madness <p_rangel@abv.bg>
2024-06-27 18:12:03 +0200

    Task 01 is done

--- Python ORM/06. Exercise - Data Operations in Django with Queries/caller.py
+++ Python ORM/06. Exercise - Data Operations in Django with Queries/caller.py
@@ -6,5 +6,22 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
+from main_app.models import Pet
+from polulate_db_script import populate_model_with_data
 
 # Create queries within functions
+def create_pet(name: str, species: str) -> str:
+    # First Way
+    pet = Pet.objects.create(
+        name=name,
+        species=species,
+    )
+    
+    # Second Way
+    # pet = Pet(name=name, species=species)
+    # pet.save()
+    
+    return f"{pet.name} is a very cute {pet.species}!"
+
+
+create_pet("Boris", "Beast")
