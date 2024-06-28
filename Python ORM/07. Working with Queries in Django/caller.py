c7aac85efee32a2ab8990642a6779c9c19c7a390
me_madness <p_rangel@abv.bg>
2024-06-28 18:53:57 +0200

    I prepare next five task

--- Python ORM/07. Working with Queries in Django/caller.py
+++ Python ORM/07. Working with Queries in Django/caller.py
@@ -9,11 +9,13 @@ django.setup()
 from main_app.models import Author, Book, Review
 
 # Create and check models
+
+# Task 01.Books Finder
 def find_book_by_find_books_by_genre_and_language(genre: str, language: str):
     books = Book.objects.filter(genre=genre, language=language)
     return books
 
-
+# Task 02.Find Authors' Nationalities
 def find_authors_nationalities():
     result = []
     # authors = Author.objects.exclude(nationality__isnull=True)
@@ -31,6 +33,7 @@ def find_authors_nationalities():
     return '\n'.join(result)    
 
 
+# Task 03.Order Books by Year
 def order_books_by_year():
     result = []
     books = Book.objects.all().order_by('publication_year', 'title')
@@ -38,5 +41,26 @@ def order_books_by_year():
         result.append(f"{b.publication_year} year: {b.title} by {b.author}")
         
     return '\n'.join(result)    
+
+
+# Task 04.Delete Review by ID
+def delete_review_by_id():
+    pass
+
+
+# Task 05.Filter Authors by Nationalities
+def filter_authors_by_nationalities():
+    pass
+
+
+# Task 06.Filter Authors by Birth Year
+def filter_authors_by_birth_year():
+    pass
+
+
+# Task 07.Change Reviewer's Name
+def change_reviewer_name():
+    pass
+
 
 # Run and print your queries
