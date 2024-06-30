85d165acc16fe19d6ac68d3b619dd1506c2777f3
me_madness <p_rangel@abv.bg>
2024-06-30 21:03:39 +0200

    I need to finish the function def show_highest_rated_art()

--- Python ORM/08. Exercise - Working with Queries in Django/caller.py
+++ Python ORM/08. Exercise - Working with Queries in Django/caller.py
@@ -6,5 +6,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
+from main_app.models import ArtworkGallery
+
+
 # Create and check models
+def show_highest_rated_art() -> str:
+    pass
+
 # Run and print your queries
