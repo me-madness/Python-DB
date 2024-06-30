1866a23e3086373405f13850b6f7015bdadd2ce7
me_madness <p_rangel@abv.bg>
2024-06-30 21:05:59 +0200

    two more functions are added in my caller.py file

--- Python ORM/08. Exercise - Working with Queries in Django/caller.py
+++ Python ORM/08. Exercise - Working with Queries in Django/caller.py
@@ -13,4 +13,13 @@ from main_app.models import ArtworkGallery
 def show_highest_rated_art() -> str:
     pass
 
+
+def bulk_create_arts(first_art: AtrworkGallery, second_art: ArtworkGallery):
+    pass
+
+
+def delete_negative_rated_arts():
+    pass
+
+  
 # Run and print your queries
