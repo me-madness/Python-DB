e93a4bac27d91c9fa3deef3f6759792608bc1629
me_madness <p_rangel@abv.bg>
2024-07-04 19:56:12 +0200

    adding new functions in my caller.py file def show_all_authors_with_their_books()

--- Python ORM/10. Exercise - Django Models Relations/caller.py
+++ Python ORM/10. Exercise - Django Models Relations/caller.py
@@ -7,4 +7,18 @@ django.setup()
 
 # Import your models here
 
+from main_app.models import Author, Book
+
+
 # Create queries within functions
+
+def show_all_authors_with_their_books() -> str:
+    authors_with_books = []
+    
+    authors = Author.objects.all().order_by("id") # select prefetch  prefetch_related
+    
+    for author in authors:
+        books = Book.objects.filter(author=author)
+        # books = author.book_set.all() # Option 2
+        
+    return "\n".join(authors_with_books)    
