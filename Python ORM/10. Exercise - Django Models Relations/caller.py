29300df4299701d5a4f805e99bb824b416d72921
me_madness <p_rangel@abv.bg>
2024-07-04 20:18:48 +0200

    Begin with task 02

--- Python ORM/10. Exercise - Django Models Relations/caller.py
+++ Python ORM/10. Exercise - Django Models Relations/caller.py
@@ -36,34 +36,3 @@ def delete_all_authors_without_books() -> None:
     Author.objects.filter(book__isnull=True).delete()
         
 
-# # Create authors
-# author1 = Author.objects.create(name="J.K. Rowling")
-# author2 = Author.objects.create(name="George Orwell")
-# author3 = Author.objects.create(name="Harper Lee")
-# author4 = Author.objects.create(name="Mark Twain")
-
-# # Create books associated with the authors
-# book1 = Book.objects.create(
-#     title="Harry Potter and the Philosopher's Stone",
-#     price=19.99,
-#     author=author1
-# )
-# book2 = Book.objects.create(
-#     title="1984",
-#     price=14.99,
-#     author=author2
-# )
-
-# book3 = Book.objects.create(
-#     title="To Kill a Mockingbird",
-#     price=12.99,
-#     author=author3
-# )
-
-# # Display authors and their books
-# authors_with_books = show_all_authors_with_their_books()
-# print(authors_with_books)
-
-# Delete authors without books
-# delete_all_authors_without_books()
-# print(Author.objects.count())
