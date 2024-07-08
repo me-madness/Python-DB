ea682480eb4a5323f4cad490eee9707f6aab10f6
me_madness <p_rangel@abv.bg>
2024-07-08 18:05:12 +0200

    The data is added in my database

--- Python ORM/12. Exercise - Models Inheritance and Customization/caller.py
+++ Python ORM/12. Exercise - Models Inheritance and Customization/caller.py
@@ -7,7 +7,26 @@ django.setup()
 
 # Import your models here
 
-
+from main_app.models import Mage, Necromancer
 
 
 # Create queries within functions
+
+mage = Mage.objects.create(
+    name="Fire Mage",
+    description="A powerful mage specializing in fire magic.",
+    elemental_power="Fire",
+    spellbook_type="Ancient Grimoire"
+)
+
+necromancer = Necromancer.objects.create(
+    name="Dark Necromancer",
+    description="A mage specializing in dark necromancy.",
+    elemental_power="Darkness", spellbook_type="Necronomicon",
+    raise_dead_ability="Raise Undead Army"
+)
+print(mage.elemental_power)
+print(mage.spellbook_type)
+print(necromancer.name)
+print(necromancer.description)
+print(necromancer.raise_dead_ability)
