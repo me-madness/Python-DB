ffcbd2e9d11974ea3bddd23b18871f5c6dbe3968
me_madness <p_rangel@abv.bg>
2024-07-09 18:23:59 +0200

    i make migration for task 03

--- Python ORM/13. Advanced Django Model Techniques/caller.py
+++ Python ORM/13. Advanced Django Model Techniques/caller.py
@@ -6,12 +6,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Restaurant, Menu
-from django.core.exceptions import ValidationError
+
 
 # Create queries within functions
 
-from main_app.models import Restaurant, Menu
-
-# Keep the data from the previous exercise, so you can reuse it
+from main_app.models import Restaurant, Menu, RestaurantReview
+from django.core.exceptions import ValidationError
 
