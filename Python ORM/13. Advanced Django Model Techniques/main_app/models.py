cf7aebc5a3f24b1c832a67c94ca92df7d8de397f
me_madness <p_rangel@abv.bg>
2024-07-09 17:31:41 +0200

    The first task is all most done

--- Python ORM/13. Advanced Django Model Techniques/main_app/models.py
+++ Python ORM/13. Advanced Django Model Techniques/main_app/models.py
@@ -1,31 +1,33 @@
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 # from django.core.exceptions import ValidationError
+
 # Create your models here.
 
 class Restaurant(models.Model):
     name = models.CharField(
         max_length=100,
         validators=[
-            MinLengthValidator
-        ]
-    #  "Name must be at least 2 characters long."   
-    #  "Name cannot exceed 100 characters."   
+            MinLengthValidator(2, message="Name must be at least 2 characters long."),
+            MaxLengthValidator(100, message="Name cannot exceed 100 characters." )
+        ] 
     )
     
     location = models.CharField(
         max_length=200,
-        
-    # "Location must be at least 2 characters long."    
-    # "Location cannot exceed 200 characters."
+        validators=[
+            MinLengthValidator(2, message="Location must be at least 2 characters long."),
+            MaxLengthValidator(200, message="Location cannot exceed 200 characters.")
+        ]
     )
     
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     
     rating = models.DecimalField(
         max_digits=3,
         decimal_places=2,
-        
-        # "Rating must be at least 0.00."
-        # "Rating cannot exceed 5.00."
+        validators=[
+            MinLengthValidator(0, message="Rating must be at least 0.00."),
+            MaxLengthValidator(5,"Rating cannot exceed 5.00.")
+        ]
     )
