d52b4de59b428bae4c2e95310c05dad604a7b6a3
me_madness <p_rangel@abv.bg>
2024-07-11 16:01:12 +0200

    I prepare everything for Task 01.Customer from lesson Exercise: Advance Django Model Techniques

--- Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
+++ Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+
+# Task 01. Customer
+class Customer(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[
+            "Name can only contain letters and spaces"
+        ]
+    )
+    
+    age = models.PositiveIntegerField(
+        validators=[
+            "Age must be greater than or equal to 18"
+        ]
+    )
+    
+    email = models.EmailField(
+        validators=[
+           : "Enter a valid email address" 
+        ]
+    )
+    
+    phone_number = models.CharField(
+        validators=[
+            "Phone number must start with a '+359' followed by 9 digits"
+        ]
+    )
+    
+    website_url = models.URLField(
+        validators=[
+            "Enter a valid URL"
+        ]
+    )
