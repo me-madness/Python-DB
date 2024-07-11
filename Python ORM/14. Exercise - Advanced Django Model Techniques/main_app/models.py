32ef4d8718b0d39a8990bf4d663f5e13afe26590
me_madness <p_rangel@abv.bg>
2024-07-11 17:42:04 +0200

    The first two part of task 01 are done

--- Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
+++ Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-# from django.core.validators import EmailValidator
-from main_app.validators import validate_name
+from django.core.validators import MinValueValidator, EmailValidator
+from main_app.validators import ValidateName
 from django.core.exceptions import ValidationError
 # Create your models here.
 
@@ -10,31 +10,30 @@ class Customer(models.Model):
     name = models.CharField(
         max_length=100,
         validators=[
-            validate_name,
-            (message="Name can only contain letters and spaces")
+            ValidateName("Name can only contain letters and spaces")
         ]
     )
     
     age = models.PositiveIntegerField(
         validators=[
-            (message="Age must be greater than or equal to 18")
+            MinValueValidator(18, message="Age must be greater than or equal to 18")
         ]
     )
     
     email = models.EmailField(
         validators=[
-            EmailValidator(message="Enter a valid email address") 
+            EmailValidator("Enter a valid email address") 
         ]
     )
     
     phone_number = models.CharField(
         validators=[
-            (message="Phone number must start with a '+359' followed by 9 digits")
+            ("Phone number must start with a '+359' followed by 9 digits")
         ]
     )
     
     website_url = models.URLField(
         validators=[
-            (message="Enter a valid URL")
+            ("Enter a valid URL")
         ]
     )
--- Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/validators.py
+++ Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/validators.py
@@ -1,6 +1,20 @@
 from django.core.exceptions import ValidationError
 
+# First Option
 def validate_name(value):
     for char in value:
         if not(char.isalpha() or char.isspace()):
             raise ValidationError("Name can only contain letters and spaces")
+        
+
+# Second Option - Much Better        
+class ValidateName:
+    def __init__(self, message: str):
+        self.message = message
+        
+    def __call__(self, value):
+        for char in value:
+            if not(char.isalpha() or char.isspace()):
+                raise ValidationError(self.message)    
+                
+                                
