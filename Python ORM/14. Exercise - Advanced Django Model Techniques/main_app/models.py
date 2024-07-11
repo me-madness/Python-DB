dcf6cd6189124d5b866677844ce7b85c67324111
me_madness <p_rangel@abv.bg>
2024-07-11 17:32:03 +0200

    I create file validators.py for my validators

--- Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
+++ Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+# from django.core.validators import EmailValidator
+from main_app.validators import validate_name
 from django.core.exceptions import ValidationError
 # Create your models here.
 
@@ -8,30 +10,31 @@ class Customer(models.Model):
     name = models.CharField(
         max_length=100,
         validators=[
-            "Name can only contain letters and spaces"
+            validate_name,
+            (message="Name can only contain letters and spaces")
         ]
     )
     
     age = models.PositiveIntegerField(
         validators=[
-            "Age must be greater than or equal to 18"
+            (message="Age must be greater than or equal to 18")
         ]
     )
     
     email = models.EmailField(
         validators=[
-           : "Enter a valid email address" 
+            EmailValidator(message="Enter a valid email address") 
         ]
     )
     
     phone_number = models.CharField(
         validators=[
-            "Phone number must start with a '+359' followed by 9 digits"
+            (message="Phone number must start with a '+359' followed by 9 digits")
         ]
     )
     
     website_url = models.URLField(
         validators=[
-            "Enter a valid URL"
+            (message="Enter a valid URL")
         ]
     )
