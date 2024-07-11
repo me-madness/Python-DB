752dc0ca7789fca8c8cf1fa597ee576a93f81d7a
me_madness <p_rangel@abv.bg>
2024-07-11 19:13:51 +0200

    Task 03 Tax work normal with out errors

--- Python ORM/14. Exercise - Advanced Django Model Techniques/caller.py
+++ Python ORM/14. Exercise - Advanced Django Model Techniques/caller.py
@@ -1,4 +1,5 @@
 import os
+from decimal import Decimal
 import django
 
 # Set up Django
@@ -6,9 +7,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Customer, Book
+from main_app.models import Customer, Book, Product, DiscountedProduct
+from decimal import Decimal
 from django.core.exceptions import ValidationError
 
 
 # Create queries within functions
+
+# Create a Product instance
 
--- Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
+++ Python ORM/14. Exercise - Advanced Django Model Techniques/main_app/models.py
@@ -129,7 +129,7 @@ class Product(models.Model):
         return f"Product: {self.name}"
     
     
-class DiscountProduct(Product):
+class DiscountedProduct(Product):
     class Meta:
         proxy = True    
   
