0ba60b4f4c61fc6c720ac4749643ebe2ee84da1a
me_madness <p_rangel@abv.bg>
2024-07-15 17:54:09 +0200

    second function is done in_price_range

--- Python ORM/16. Exercise - Advanced Queries in Django/main_app/managers.py
+++ Python ORM/16. Exercise - Advanced Queries in Django/main_app/managers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from django.db.models import QuerySet
 
@@ -7,3 +8,9 @@ class RealEstateListingManager(models.Manager):
 
     def by_property_type(self, property_type: str) -> QuerySet:
         return self.filter(property_type=property_type)
+    
+    def in_price_range(self, min_price: Decimal, max_price: Decimal) -> QuerySet:
+        return self.filter(price__range=[min_price, max_price])
+    
+    def with_bedrooms(self, bedrooms_count: int) -> QuerySet:
+        return self.filter()
