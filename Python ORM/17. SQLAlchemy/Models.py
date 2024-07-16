6e1bc05ddc77a97ea5ca4c9e92738dab2225e9f5
me_madness <p_rangel@abv.bg>
2024-07-16 19:53:53 +0200

    I create my first class with AQLAlchemy

--- Python ORM/17. SQLAlchemy/Models.py
+++ Python ORM/17. SQLAlchemy/Models.py
@@ -1,7 +1,17 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
-CONNECTION_STRING='<dialect>+<drive>://<username>:<password>@<host>:<port>/database'
+# CONNECTION_STRING='<dialect>+<drive>://<username>:<password>@<host>:<port>/database'
 CONNECTION_STRING='postgresql+psycopg2://postgres:Dragulia131@@localhost:5432/sqlalchemy'
 
 engine = create_engine('CONNECTION_STRING')
+Base = declarative_base()
+
+class Employee(Base):
+    __tablename__ = 'employees'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    age = Column(Integer)
+    
+Base.metadata.create_all(engine)    
