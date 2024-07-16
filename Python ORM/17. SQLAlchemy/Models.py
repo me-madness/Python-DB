20b9cda4f7a992ce435de772cf3391c390d858eb
me_madness <p_rangel@abv.bg>
2024-07-16 19:43:23 +0200

    I create the connection string to my database

--- Python ORM/17. SQLAlchemy/Models.py
+++ Python ORM/17. SQLAlchemy/Models.py
@@ -1,2 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
+
+CONNECTION_STRING='<dialect>+<drive>://<username>:<password>@<host>:<port>/database'
+CONNECTION_STRING='postgresql+psycopg2://postgres:Dragulia131@@localhost:5432/sqlalchemy'
+
+engine = create_engine('CONNECTION_STRING')
