4edf5c33dc8f427941d6515e0b8f87042c49bca7
me_madness <p_rangel@abv.bg>
2024-07-22 19:17:29 +0200

    first function in Task 04 def get_profiles() is done

--- Python ORM/Exam Preparation 22.07.2024/caller.py
+++ Python ORM/Exam Preparation 22.07.2024/caller.py
@@ -1,5 +1,6 @@
 import os
 import django
+from django.db.models import Q
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -7,4 +8,29 @@ django.setup()
 
 # Import your models here
 
+from main_app.models import Profile
+
+
 # Create queries within functions
+def get_profiles(search_string=None) -> str:
+    if search_string is None:
+        return ""
+    
+    profiles = Profile.objects.filter(
+        Q(full_name__icontains=search_string)
+            |
+        Q(email__icontains=search_string)
+            |
+        Q(phone_number__icontains=search_string)
+    )
+    
+    if not profiles.exists():
+        return ""
+    
+    return '\n'.join(
+        f"Profile: {p.full_name}, email: {p.email}, phone_number: {p.phone_number}, orders: {p.orders.count()}"
+        for p in profiles       
+    )
+    
+    
+print(get_profiles("ra"))    
