f2e5714780a433e950996a1f258fc8545e2598ed
me_madness <p_rangel@abv.bg>
2024-07-22 18:51:22 +0200

    I import Count from django.db.models

--- Python ORM/Exam Preparation 22.07.2024/main_app/managers.py
+++ Python ORM/Exam Preparation 22.07.2024/main_app/managers.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models import Count
 
 
 class ProfileManager(models.Manager):
-    def get_regular_customers():
-        
+    def get_regular_customers(self):
+        return self.annotate(
+            orders_count=Count('orders')
+        )
