456b7a1f1141f0e321c645886ad832f84e20d816
me_madness <p_rangel@abv.bg>
2024-07-22 18:10:27 +0200

    next two class are added in my admin file

--- Python ORM/Exam Preparation 22.07.2024/main_app/admin.py
+++ Python ORM/Exam Preparation 22.07.2024/main_app/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from main_app.models import Profile
+from main_app.models import Profile, Product, Order
 
 
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
     pass
