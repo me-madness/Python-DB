5d65c4e2a1f54c679e2179827e1cfa826bc64af8
me_madness <p_rangel@abv.bg>
2024-07-22 17:38:06 +0200

    I begin with the exam preparation

--- Python ORM/Exam Preparation 22.07.2024/main_app/models.py
+++ Python ORM/Exam Preparation 22.07.2024/main_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
+
+class DateTime(models.Model):
+    creation_date = models.DateTimeField(
+        auto_now_add=True
+    )
 
 # Create your models here.
+class Profile(models.Model):
+    full_name = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(2)
+        ]
+    )
+    
+    email = models.EmailField()
+    
+    phone_number = models.CharField(
+        max_length=15,
+    )
+    
+    address = models.TextField()
+    
+    is_active = models.BooleanField(
+        default=False,
+    )
+    
