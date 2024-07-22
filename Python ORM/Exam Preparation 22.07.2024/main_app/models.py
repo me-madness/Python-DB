a22a441f14374b99a544908d1947e0f71c377deb
me_madness <p_rangel@abv.bg>
2024-07-22 17:40:00 +0200

    First model Profile is done

--- Python ORM/Exam Preparation 22.07.2024/main_app/models.py
+++ Python ORM/Exam Preparation 22.07.2024/main_app/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+# Create your models here.
 class DateTime(models.Model):
+    class Meta:
+        abstract = True
+        
     creation_date = models.DateTimeField(
         auto_now_add=True
     )
 
-# Create your models here.
-class Profile(models.Model):
+
+class Profile(DateTime):
     full_name = models.CharField(
         max_length=100,
         validators=[
