133ec7b2dc33fe97064ba465c4f5c6e218ff7d7c
me_madness <p_rangel@abv.bg>
2024-07-22 18:16:34 +0200

    all data i s added in my admin file

--- Python ORM/Exam Preparation 22.07.2024/main_app/admin.py
+++ Python ORM/Exam Preparation 22.07.2024/main_app/admin.py
@@ -5,14 +5,18 @@ from main_app.models import Profile, Product, Order
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['full_name', 'email', 'phone_number', 'is_active']
+    search_fields = ['full_name', 'email']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ['name', 'price', 'in_stock', 'is_available']
+    list_filter = ['is_available']
+    search_fields = ['name']
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['profile', 'total_price', 'creation_date', 'is_completed']
+    list_filter = ['is_completed']
+    search_fields = ['full_name']
