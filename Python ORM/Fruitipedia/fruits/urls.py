85296e964cda082d77be3eb9b3ecd7a391d324e3
me_madness <p_rangel@abv.bg>
2024-07-23 20:18:25 +0200

    i add some new url for fruits app

--- Python ORM/Fruitipedia/fruits/urls.py
+++ Python ORM/Fruitipedia/fruits/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from fruits import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')     
+    path('', views.index, name='index'),     
+    path('dashboard/', views.dashboard, name='dashboard'),     
+    path('create-fruit/', views.create_view, name='create_fruit'),    
+    path('create-category/', views.create_view, name='create_category'),     
+    path('<int:pk>/', include([
+        path('edit-fruit/', views.edit_view, name='edit_fruit'), 
+        path('delete-fruit/', views.delete_view, name='delete_fruit'),     
+        path('detail-fruit/', views.details_view, name='details_fruit'),     
+        ]))     
 ]
