3b60951c28f2d8cfa6351f234173597d341b3a15
me_madness <p_rangel@abv.bg>
2024-07-30 17:51:27 +0200

    I delete one folder from my course details

---
+++ Python ORM/19. Workshop - Fruitipedia/fruits/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from fruits import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),     
+    path('dashboard/', views.dashboard, name='dashboard'),     
+    path('create-fruit/', views.CreateFruitView.as_view(), name='create_fruit'),    
+    path('create-category/', views.create_category, name='create_category'),     
+    path('<int:pk>/', include([
+        path('edit-fruit/', views.edit_view, name='edit_fruit'), 
+        path('delete-fruit/', views.DeleteFruitForm.as_view(), name='delete_fruit'),     
+        path('detail-fruit/', views.details_view, name='details_fruit'),     
+        ]))     
+]
