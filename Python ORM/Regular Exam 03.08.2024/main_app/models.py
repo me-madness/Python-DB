defcba6b75a8a4edaa5791375d2acdc3522625c1
me_madness <p_rangel@abv.bg>
2024-08-03 13:16:48 +0200

    first three value are added

--- Python ORM/Regular Exam 03.08.2024/main_app/models.py
+++ Python ORM/Regular Exam 03.08.2024/main_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class Astounaut(models.Model):
+    name = models.CharField(
+        max_length=120,
+        validators=[
+            MinLengthValidator(2)
+        ]
+    )
+    
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True,
+        validators=[
+            #Must contain only digits
+        ]
+    )
+    
+    is_active = models.BooleanField(
+        default=True,
+    )
+    
+    
